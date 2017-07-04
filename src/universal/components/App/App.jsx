89b0d40fd1ddc046d21adb1ce81e8df408239bae
MartinCerny-awin <martin.cerny@affiliatewindow.com>
2017-07-04 20:23:43 +0100

    Add Navigation to all pages

--- src/universal/components/App/App.jsx
+++ src/universal/components/App/App.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Navigation from 'components/Navigation/Navigation';
 import 'styles/global.css';
 import styles from './App.css';
 
 const App = props =>
   (<div className={styles.app}>
 
+    {props.location.pathname === '' ? <Navigation /> : ''}
     {props.children}
 
   </div>);
 
 App.propTypes = {
   children: PropTypes.element.isRequired,
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
 };
 
 export default App;
